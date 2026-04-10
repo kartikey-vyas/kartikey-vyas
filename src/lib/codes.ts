76751a544641818136d2bfc21d2624f02acaefe8
Kartikey Vyas <kartikeyvyas23@gmail.com>
2026-04-10 20:26:58 +0200

    chore: move split app to its own repo

--- src/lib/codes.ts
+++
@@ -1,9 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-// 6-char uppercase alphanumeric, excluding ambiguous chars (0/O, 1/I/L).
-const alphabet = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
-const generate = customAlphabet(alphabet, 6);
-
-export function generateJoinCode(): string {
-  return generate();
-}
